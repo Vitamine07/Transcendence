dfa1166665f5e0ec43ea3dd4b40b29be78990912
Mehdi El Warrad <meel-war@k1r2p3.42mulhouse.fr>
2026-04-16 18:54:33 +0200

    Structuration du Backend, healthcheck du back, ajout d un prefixglobal api et activation du CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api'); // mettre le prefix globale 'api' pour toutes les routes
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // CORS autorise un port qui differe du back-end, donc en gros autorise le front end a appeler le back en
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
